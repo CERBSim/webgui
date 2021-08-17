a5cc3824f926cf1a4df2bb7d9100384c20ac4d46
Matthias Hochsteger <mhochsteger@cerbsim.com>
2021-08-17 11:46:10 +0200

    build version with external three.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,52 @@
 const webpack = require('webpack');
 const path = require('path');
-const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 var package = require('./package.json');
+const three_version = package.dependencies.three.replace('^','');
 
-const config = {
-  entry: './src/index.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'webgui.js',
-    library: package.name,
-    libraryTarget: 'umd',
-    globalObject: 'this',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts(x)?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: [
-      '.tsx',
-      '.ts',
-      '.js'
-    ]
-  },
-  plugins: [
-    new LodashModuleReplacementPlugin
-  ]
+function getConfig() {
+    return config = {
+        entry: './src/index.ts',
+        externalsType: 'script',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'webgui.js',
+            library: package.name,
+            libraryTarget: 'umd',
+            globalObject: 'this',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts(x)?$/,
+                    loader: 'ts-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader'
+                    ]
+                }
+            ]
+        },
+        resolve: {
+            extensions: [
+                '.tsx',
+                '.ts',
+                '.js'
+            ]
+        },
+    };
+}
+
+var html_config = getConfig();
+html_config.output.filename = "webgui_external_three.js";
+html_config.externals = {
+'@jupyter-widgets/base': '@jupyter-widgets/base',
+    three: [`https://unpkg.com/three@${three_version}/build/three.min.js`, "THREE"],
 };
 
-module.exports = config;
+module.exports = [
+    getConfig(), html_config
+];
