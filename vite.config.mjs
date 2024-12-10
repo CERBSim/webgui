e3deb36d43128735874ddf8d1870ba53d953fd91
Matthias Hochsteger <mhochsteger@cerbsim.com>
2024-12-10 16:16:46 +0100

    Fix watch mode for development

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,39 @@
 import { defineConfig } from 'vite';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
+import { readFileSync, readdirSync, writeFileSync } from 'fs';
+import { join, resolve } from 'path';
+
+function buildShaders(watcher) {
+  const src_dir = 'src/shader';
+
+  const shader_codes = {};
+  const files = readdirSync(src_dir);
+  for (const file of files) {
+    const buff = readFileSync(join(src_dir, file));
+    const code = buff.toString('base64');
+    shader_codes[file] = code;
+    watcher.addWatchFile(resolve(__dirname, join(src_dir, file)));
+  }
+
+  const date = new Date().toISOString().replace('T', ' ').replace('Z', '');
+  let shader_code = `
+export const BUILD_TIME = "${date}";
+export const shaders = ${JSON.stringify(shader_codes, null, 2)};
+`;
+  writeFileSync('src/shaders.ts', shader_code);
+}
+
 export default defineConfig({
-  plugins: [cssInjectedByJsPlugin()],
+  plugins: [
+    {
+      name: 'watch-external',
+      async buildStart() {
+        buildShaders(this);
+      },
+    },
+    cssInjectedByJsPlugin(),
+  ],
   build: {
     lib: {
       entry: './src/index.ts',
@@ -11,6 +42,12 @@ export default defineConfig({
       formats: ['iife'],
     },
     minify: true,
+    // only watch in dev mode
+    watch: process.argv.includes('development')
+      ? {
+          exclude: 'src/shaders.ts',
+        }
+      : undefined,
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
